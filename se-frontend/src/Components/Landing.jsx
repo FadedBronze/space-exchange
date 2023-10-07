34c963c0785a0a9a7a69c8cd1378a4f8e9a1af36
Cyronite <prasunsharma@icloud.com>
2023-10-07 12:16:48 -0400

    i am a monkey

--- se-frontend/src/Components/Landing.jsx
+++ se-frontend/src/Components/Landing.jsx
@@ -1,3 +1,12 @@
+import Bg from "../assets/landingbg.png";
+
 export default function LandingPage() {
-  return <div>hello</div>;
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <img
+        style={{ width: "100%", height: "100%", padding: "0" }}
+        src={Bg}
+      ></img>
+    </div>
+  );
 }
