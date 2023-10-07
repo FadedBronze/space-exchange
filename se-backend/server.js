35bef3b64270d544c4f594bf7944459484d6d15a
Sidak <magicalsidak@gmail.com>
2023-10-07 12:46:08 -0400

    server can write to db

--- se-backend/server.js
+++ se-backend/server.js
@@ -1,23 +1,46 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const uri = process.env.URI
+const client = new MongoClient(uri);
+
+let personDocument = {
+    "hi": "test"
+}
 
 app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:5173',
 }));
 
+async function connectToMongoDB() {
+    try {
+        // Connect to the Atlas cluster
+        await client.connect();
+    } catch (err) {
+        console.log(err.stack);
+    }
+}
+connectToMongoDB().catch(console.dir);
+
+const saveData = async (data) => {
+    const db = client.db("main");
+    const col = db.collection("main");
+    const p = await col.insertOne(personDocument);
+}
+saveData(personDocument);
+
 app.post('/home', (req, res) => {
-    const requestData = req.body; // Access POST data from the request body
+    const requestData = req.body;
     console.log('Received POST data:', requestData);
 
-    // Respond with a message
-    setTimeout(() => {
-        res.status(200).json({ "test": "test" });
-    }, 2000);
+    res.status(200).json({});
 });
 
-app.listen(port, () => {
+app.listen(3000, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
