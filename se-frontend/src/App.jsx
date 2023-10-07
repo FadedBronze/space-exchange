a3ea4cb1ca7559f720801b5961990af3181f80e1
Sidak <magicalsidak@gmail.com>
2023-10-07 11:01:41 -0400

    everything basic works

--- se-frontend/src/App.jsx
+++ se-frontend/src/App.jsx
@@ -1,3 +1,6 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import axios from 'axios';
+
 function App() {
   useEffect(() => {
     axios.post('http://localhost:3000/home', {})
