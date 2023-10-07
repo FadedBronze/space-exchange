06a3d732d488ea00b51c939179020e3e904665ed
FadedBronze <TheSamyatGautam@gmail.com>
2023-10-07 12:31:47 -0400

    centered image

--- se-frontend/src/Components/Landing.jsx
+++ se-frontend/src/Components/Landing.jsx
@@ -1,12 +1,10 @@
-import Bg from "../assets/landingbg.png";
+import Bg from "../assets/landingBg.png";
 
 export default function LandingPage() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <img
-        style={{ width: "100%", height: "100%", padding: "0" }}
-        src={Bg}
-      ></img>
+    <div className="w-full h-screen flex justify-center items-center relative">
+      <img className="object-cover w-full h-full object-center" src={Bg}></img>
+      <div className="absolute top-0 left-0 w-full h-full text-white">hwlo</div>
     </div>
   );
 }
