6e0cfda6b641e31938c8951dba420131dade9351
FadedBronze <TheSamyatGautam@gmail.com>
2023-10-07 15:52:28 -0400

    moved gitignore

--- se-frontend/src/Components/Landing.jsx
+++ se-frontend/src/Components/Landing.jsx
@@ -3,8 +3,8 @@ import Bg from "../assets/landingBg.png";
 export default function LandingPage() {
   return (
     <div className="w-full h-screen flex justify-center items-center relative">
-      <img className="w-full h-full object-contain object-cover" src={Bg}></img>
-      {/* <div className="absolute top-0 left-0 w-full h-full text-white">hwlo</div> */}
+      <img className="w-full h-full object-center object-cover" src={Bg}></img>
+      <div className="absolute top-0 left-0 w-full h-full text-white">hwlo</div>
     </div>
   );
 }
