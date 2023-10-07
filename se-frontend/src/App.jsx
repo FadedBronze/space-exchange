02be61ffd4e04b6119590b715e5dab126ae0fd82
Sidak <magicalsidak@gmail.com>
2023-10-07 10:53:11 -0400

    got server and cors config

--- se-frontend/src/App.jsx
+++ se-frontend/src/App.jsx
@@ -1,4 +1,17 @@
 function App() {
+  useEffect(() => {
+    axios.post('http://localhost:3000/home', {})
+      .then(response => {
+        // Handle the successful response
+        // console.log('Response:', response.data);
+        console.log('Response:', response.data);
+      })
+      .catch(error => {
+        // Handle the error
+        console.error('Error:', error);
+      });
+  }, [])
+
   return (
     <>
       <div className="text-red-600">hello red</div>
